48c9b9f9b6d67aabb14d7e3109bd496ea50cc7e6
Emanuele Scarabattoli <scarabattoli@unicc.org>
2023-03-27 21:33:28 +0200

    beep

---
+++ beep/src/main.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+let beeperPin = undefined
+
+try {
+  const Gpio = require("onoff").Gpio;
+  beeperPin = new Gpio(21, "out");
+} catch (error) {
+  fs.writeFileSync("/home/emanuele/Downloads/raspy/error.txt", error.message);
+}
+
+const beep = () => {
+  try {
+    beeperPin.writeSync(1);
+    setTimeout(() => {
+      beeperPin.writeSync(0);
+    }, 500);
+    return "done";
+  } catch (error) {
+    fs.writeFileSync("~/Downloads/raspy/error.txt", error.message);
+  }
+}
